0618dcf54fcfbf86c04408184f7912f309221298
roxy_huang <sixin_huang@outlook.com>
2018-01-03 14:17:47 +0800

    update log

--- utils/logUtils.js
+++ utils/logUtils.js
@@ -1,13 +1,27 @@
+/*
+
+Author : Neo_Huang
+
+Log levels:
+  error: 0,
+   warn: 1,
+   info: 2,
+
+You can use eg: logger.error('message');
+
+*/
+
 const { createLogger, format, transports } = require('winston');
 const moment = require('moment');
 const expressWinston = require('express-winston');
+require('winston-daily-rotate-file');
 
-// log levels
-// {
-//   error: 0,
-//    warn: 1,
-//    info: 2,
-// }
+// const transport = new (transports.DailyRotateFile)({
+//   filename: './access.log',
+//   datePattern: 'yyyy-MM-dd.',
+//   prepend: true,
+//   level: 'info'
+// });
 
 const logger = createLogger({
   transports: [
@@ -24,6 +38,7 @@ const accessLog = createLogger({
       accessLogFormat
   ),
   transports: [
+    // transport
     new transports.File({json: true, filename: '/Users/neo/opt/log/access.log' })
   ]
 });
