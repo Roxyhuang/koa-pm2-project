c808db71dbb90de441567382b6fe916e208d6ff4
roxy_huang <sixin_huang@outlook.com>
2018-01-03 16:12:19 +0800

    update daily-rotate-file and modify winston 2.0.0

--- utils/logUtils.js
+++ utils/logUtils.js
@@ -9,37 +9,51 @@ Log levels:
 
 You can use eg: logger.error('message');
 
+Winston 2.0
+
 */
 
-const { createLogger, format, transports } = require('winston');
+const { Logger, format, transports } = require('winston');
 const moment = require('moment');
 const expressWinston = require('express-winston');
 require('winston-daily-rotate-file');
 
-// const transport = new (transports.DailyRotateFile)({
-//   filename: './access.log',
-//   datePattern: 'yyyy-MM-dd.',
-//   prepend: true,
-//   level: 'info'
-// });
+const transport = new (transports.DailyRotateFile)({
+  filename: './access.log',
+  datePattern: '.yyyy-MM-dd-HH-mm',
+  prepend: false,
+  level: 'info'
+});
 
-const logger = createLogger({
+const logger = new Logger({
   transports: [
     new transports.Console(),
   ]
 });
 
-const accessLogFormat = format.printf(info => {
-  return `${moment(info.timestamp).format('YYYY-MM-DD HH:mm Z')} level: ${info.level}  message: ${info.message} request: ${JSON.stringify(info.req)} response: ${JSON.stringify(info.res)}`;
+logger.configure({
+  transports: [
+    new (require('winston-daily-rotate-file'))({
+      filename: './access.log',
+      datePattern: 'yyyy-MM-dd.',
+      prepend: true,
+      level: 'info'
+    })
+  ]
 });
 
-const accessLog = createLogger({
-  format: format.combine(
-      accessLogFormat
-  ),
+// const accessLogFormat = format.printf(info => {
+//   return `${moment(info.timestamp).format('YYYY-MM-DD HH:mm Z')} level: ${info.level}  message: ${info.message} request: ${JSON.stringify(info.req)} response: ${JSON.stringify(info.res)}`;
+// });
+
+const accessLog = new Logger({
+  formatter: function (info) {
+    return `${moment(info.timestamp).format('YYYY-MM-DD HH:mm Z')} level: ${info.level}  message: ${info.message} request: ${JSON.stringify(info.req)} response: ${JSON.stringify(info.res)}`;
+
+  },
   transports: [
-    // transport
-    new transports.File({json: true, filename: '/Users/neo/opt/log/access.log' })
+    // new transports.File({json: true, filename: '/Users/neo/opt/log/access.log' })
+    transport,
   ]
 });
 
